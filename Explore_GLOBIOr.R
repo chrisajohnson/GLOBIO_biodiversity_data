71483f003a84cb4a8c4816517a2f8496d147ab57
Chris Johnson <80363208+chrisajohnson@users.noreply.github.com>
2026-06-11 14:17:19 +0200

    Finally got R package installed correctly (had to zip the folder first)

---
+++ Explore_GLOBIOr.R
@@ -0,0 +1,7 @@
+# LOAD GLOBIOr
+# Initially install the package from a ZIP folder
+# install.packages("./GLOBIOr.zip", repos = NULL, type = "binary")
+library(GLOBIOr)
+
+# List all functions and variables
+ls("package:GLOBIOr")
